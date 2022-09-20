77e586f04dc110d96c88d36b3406682df7b76087
Spongibob <110832267+Spongibob@users.noreply.github.com>
2022-09-20 07:45:53 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -1,11 +1,11 @@
- /**
-  * Basic Hello World Program
-  * /
+/**
+ * Basic Hello World Program
+ * /
 
-  int main(int argc, char **argv){
+ int main(int argc, char **argv) {
 
     printf("Hello, I'm Ehab Helmy!\n");
-    printf("Major is Computer Science!\n");
+    printf("Major is computer Science!\n");
 
     return 0;
-  }
+ }
